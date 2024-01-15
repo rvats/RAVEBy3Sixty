036241640c04a1f00788c1094542fc37ca4b619a
mrashidkhan <105515508+mrashidkhan@users.noreply.github.com>
2024-01-16 02:22:08 +0500

    Add files via upload

---
+++ animations.js
@@ -0,0 +1,4 @@
+gsap.from(".container", { opacity: 0, duration: 1, delay: 0.5, y: -50, ease: "power2.out" });
+gsap.from("h1", { opacity: 0, duration: 1, delay: 1, y: -50, ease: "power2.out" });
+gsap.from("p", { opacity: 0, duration: 1, delay: 1.5, y: -50, ease: "power2.out" });
+gsap.from(".social-links a", { opacity: 0, duration: 1, delay: 2, y: 50, ease: "power2.out", stagger: 0.2 });
